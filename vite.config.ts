5c4c39e576cc2fcc5a4a1303c9dc9c73e97acde9
gaearon <lijq1103@163.com>
2022-05-14 21:56:03 +0800

    feat: cssmodule & scss

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  css:{
+	  modules: {
+      generateScopedName: '[name]__[local]___[hash:base64:5]',
+      hashPrefix: 'prefix',
+    },
+	}
 })
