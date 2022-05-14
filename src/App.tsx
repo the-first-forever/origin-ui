1ae6d30f0dcc464339d54a81656d4e108d8b8b3a
Jiaqi Li <102645376+tripalink-lijiaqi@users.noreply.github.com>
2022-05-15 00:22:32 +0800

    feat: variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import { useState } from 'react'
-import styles from './test.module.scss'
+import './styles/index.scss'
 
 function App() {
   return (
-    <div className={styles.base}>
-      hello
+    <div>
+      <a href="">go to baidu</a>
+      <ul>
+        <li>
+          asd
+        </li>
+        <li>
+          gfsg
+        </li>
+      </ul>
+      <button>123</button>
     </div>
   )
 }
