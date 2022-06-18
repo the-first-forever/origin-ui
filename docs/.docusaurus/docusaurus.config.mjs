e7cbc4628a0945cb0840ff7a8c6c6ca101db79d3
Jiaqi.li <lijq1103@163.com>
2022-06-18 23:27:08 +0800

    Feat docs (#6)
    * feat: script
    
    * fix: docs build error

--- docs/.docusaurus/docusaurus.config.mjs
+++ docs/.docusaurus/docusaurus.config.mjs
@@ -64,7 +64,7 @@ export default {
           "items": [
             {
               "label": "Tutorial",
-              "to": "/123/intro"
+              "to": "/docs/intro"
             }
           ]
         },
